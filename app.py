157462a0c5082ddb72fba35618a5904575086cc6
Aha <matchmielarz31@onet.pl>
2025-06-05 15:12:38 +0200

    app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import uuid
 import os
 from dotenv import load_dotenv
 
-# Wczytaj dane z .env
+
 load_dotenv()
 
 
 LANGFUSE_PUBLIC_KEY = os.getenv("LANGFUSE_PUBLIC_KEY")
 LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")
-LANGFUSE_HOST = os.getenv("LANGFUSE_HOST")  # nowa zmienna
+LANGFUSE_HOST = os.getenv("LANGFUSE_HOST")  
 
 langfuse = Langfuse(
     public_key=LANGFUSE_PUBLIC_KEY,
     secret_key=LANGFUSE_SECRET_KEY,
-    host=LANGFUSE_HOST   # podajesz host z .env
+    host=LANGFUSE_HOST   
 )
 # ==== WCZYTANIE MODELU ====
 model = load_model("diabetes_model")
